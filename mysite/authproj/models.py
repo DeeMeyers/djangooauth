4a8ffc36c3825d7fec48cd123a771626a90843d6
Dee <justdmeyers@gmail.com>
2020-06-30 13:21:45 -0400

    ate cookies, roughed out model

--- mysite/authproj/models.py
+++ mysite/authproj/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Goal(models.Model):
+    current = models.IntegerField(blank=True, null=True)
+    previous = models.IntegerField(blank=True, null=True)
+    goal = models.IntegerField(blank=True, null=True)
+    currentLogin = models.DateTimeField(auto_now_add= True)
+    lastLogin = models.DateTimeField()
