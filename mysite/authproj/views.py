9e110a238d7866de5582e6863063b1e54f5aa224
Dee <justdmeyers@gmail.com>
2020-07-02 12:30:41 -0400

    passing context for meter

--- mysite/authproj/views.py
+++ mysite/authproj/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Goal
 
 
 def index(request):
     return render(request, 'base.html')
 
 def userpage(request):
-    return render(request, 'core/page1.html')
+    goal = Goal.objects.get(user=request.user)
+    return render(request, 'core/page1.html', {'goal': goal})
