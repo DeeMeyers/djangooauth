b0966203449581b45e79f78194f559d4c4809a03
Dee <justdmeyers@gmail.com>
2020-07-16 14:21:26 -0400

    h10 error

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -67,7 +67,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'termplates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
